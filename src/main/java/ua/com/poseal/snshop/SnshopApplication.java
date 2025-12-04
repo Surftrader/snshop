b9efe6c782fba2d1a4d21bfd50449ddddb1b36d7
Sergiy Poddubka <poseal@gmail.com>
2025-12-05 01:04:50 +0200

    init project

---
+++ src/main/java/ua/com/poseal/snshop/SnshopApplication.java
@@ -0,0 +1,13 @@
+package ua.com.poseal.snshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnshopApplication.class, args);
+	}
+
+}
