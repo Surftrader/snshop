0fbaad53f11550cd52fbad9263c064f8f5c53bf8
Sergiy Poddubka <poseal@gmail.com>
2025-12-05 04:12:15 +0200

    add field to product, add link for cors

--- src/main/java/ua/com/poseal/snshop/model/ProductEntity.java
+++ src/main/java/ua/com/poseal/snshop/model/ProductEntity.java
@@ -19,6 +19,9 @@ public class ProductEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "name")
+    private String name;
+
     @Column(name = "description")
     private String description;
 
